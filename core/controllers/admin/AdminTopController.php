cf3fa61dd4ec71d66ad3c039c4b6c905493f3bd8
2bo0 <2bo0webmaster@gmail.com>
2020-07-05 18:52:54 +0900

    add template engine

--- core/controllers/admin/AdminTopController.php
+++ core/controllers/admin/AdminTopController.php
@@ -3,5 +3,16 @@ class AdminTopController extends AdminAppController
 {
     public function index() {
         echo "admin top page.";
+
+        $loader = new \Twig\Loader\FilesystemLoader(APP_ROOT_PATH . 'templates');
+        $twig = new \Twig\Environment($loader, [
+            'cache' => TMP_ROOT_PATH . 'twig/compilation_cache',
+        ]);
+        $template = $twig->load('admin/sample.html.twig');
+        $data = array(
+            'title' => 'sample',
+            'message'  => 'My Webpage1!',
+        );
+        echo $template->render($data);
     }
 }
--- public/index.php
+++ public/index.php
@@ -5,9 +5,16 @@ if ( !defined('APP_ROOT_PATH') )
 if ( !defined('CORE_ROOT_PATH') )
     define('CORE_ROOT_PATH', dirname(dirname(__FILE__)) . '/core/');
 
+if ( !defined('TMP_ROOT_PATH') )
+    define('TMP_ROOT_PATH', dirname(dirname(__FILE__)) . '/tmp/');
+
+if ( !defined('VENDOR_ROOT_PATH') )
+    define('VENDOR_ROOT_PATH', dirname(dirname(__FILE__)) . '/vendor/');
+
 session_start();
 
-require_once CORE_ROOT_PATH . '/system/AutoClassLoader.php';
-require_once CORE_ROOT_PATH . '/system/AutoConfigLoader.php';
+require_once VENDOR_ROOT_PATH . 'autoload.php';
+require_once CORE_ROOT_PATH . 'system/AutoClassLoader.php';
+require_once CORE_ROOT_PATH . 'system/AutoConfigLoader.php';
 
 Router::go();
